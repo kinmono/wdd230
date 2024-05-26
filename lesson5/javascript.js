972ab060423f82a73505f6dfbb46c0b5e1ebcb18
kinmono <alkynes2010@gmail.com>
2024-05-26 16:46:36 +0100

    update

---
+++ lesson5/javascript.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener("click", ()=>{
+    if(input.value ==""){
+        alert("Input Feild Must not be Empty");
+        input.focus();
+    }
+
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+
+    li.textContent = input.value;
+    button.textContent = "❌"
+
+    li.append(button);
+    list.append(li);
+    button.addEventListener("click",()=>{
+        list.removeChild(li);
+    })
+})
