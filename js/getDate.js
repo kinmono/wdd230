e010bb34a883124fbba48e0fa2e626ff3b9ec169
donsoj1st <donsoj1st@byui.edu>
2024-10-16 11:18:14 +0100

    update

--- js/getDate.js
+++ js/getDate.js
@@ -1,31 +1,40 @@
 // Webfont Loader
 WebFont.load({
-    google: {
-      families: [
-         'Lato'
-      ]
-    }
-  });
-  
-  
-  
-  // Select elements from DOM
-  const currentYearEl = document.querySelector("#currentYear");
-  const lastUpdatedEl = document.querySelector("#lastUpdated span");
-  
-  // Find current year
-  const now = new Date();
-  const currentYear = now.getFullYear();
-  
-  // Update DOM element
-  currentYearEl.textContent = currentYear.toString();
-  
-  // Find Last Update Time
-  const lastUpdate = new Date(document.lastModified);
-  const formattedlastUpdate = new Intl.DateTimeFormat("en-GB", {
-    dateStyle: "short",
-    timeStyle: "medium",
-  }).format(lastUpdate);
-  
-  // Update DOM element
-  lastUpdatedEl.textContent = formattedlastUpdate;
+  google: {
+    families: ["Lato"],
+  },
+});
+
+// Select elements from DOM
+const currentYearEl = document.querySelector("#currentYear");
+const lastUpdatedEl = document.querySelector("#lastUpdated span");
+
+// Find current year
+const now = new Date();
+const currentYear = now.getFullYear();
+
+// Update DOM element
+currentYearEl.textContent = currentYear.toString();
+
+// Find Last Update Time
+const lastUpdate = new Date(document.lastModified);
+const formattedlastUpdate = new Intl.DateTimeFormat("en-GB", {
+  dateStyle: "short",
+  timeStyle: "medium",
+}).format(lastUpdate);
+
+// Update DOM element
+lastUpdatedEl.textContent = formattedlastUpdate;
+
+// Handle Responsive Menu
+const menuBtn = document.querySelector("#menuBtn");
+const menu = document.querySelector("#menu");
+
+menuBtn.addEventListener(
+  "click",
+  () => {
+    menu.classList.toggle("show");
+    menuBtn.classList.toggle("show");
+  },
+  false
+);
