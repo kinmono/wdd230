d4458d1cf84b9b3a45c66dc267d76fbfdc4e41ae
donsoj1st <donsoj1st@byui.edu>
2024-11-23 09:36:54 +0100

    update

--- scripts/getDate.js
+++ scripts/getDate.js
@@ -41,3 +41,25 @@ menuBtn.addEventListener(
   },
   false
 );
+async function getWeather() {
+  const res = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?lat=6.632192667531344&lon=3.3806072978815394&appid=7a24f23ff0f6ddd9ae6572431e5df1b0&units=imperial"
+  );
+  const data = await res.json();
+  const temp = data.main.temp;
+  const desc = data.weather[0].description;
+  const icon = data.weather[0].icon;
+
+  // Update DOM
+  document.getElementById("city").textContent = data.name;
+  document.getElementById(
+    "temperature"
+  ).textContent = `Temperature: ${temp.toFixed(1)} °F`;
+  document.getElementById("description").textContent = `Condition: ${desc}`;
+  document.getElementById(
+    "weather-icon"
+  ).src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+  document.getElementById("weather-icon").alt = desc;
+}
+
+getWeather();
